ad727d975513bd571f8ffd66b3d36f6550568f43
souer25 <souer25@gmail.com>
2025-12-06 12:29:02 +0800

    ython 学习阶段总结 - 基础核心 (Day 1 - Day 2)

---
+++ 30-2.py
@@ -0,0 +1,4 @@
+project_langs_1 = {"Python", "SQL", "Git"}
+project_langs_2 = {"Python", "JavaScript", "HTML"}
+common_langs = project_langs_1.intersection(project_langs_2)
+print(common_langs)  # Output: {'Python'}
